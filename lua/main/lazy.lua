d5442fd7283b36da96311e0b7e2ff4a7250d17d3
Jani Haakana <jani.haakana@icloud.com>
2023-11-17 20:26:45 +0200

    add nvim-surround

--- lua/main/lazy.lua
+++ lua/main/lazy.lua
@@ -63,4 +63,16 @@ require('lazy').setup({
     'laytan/cloak.nvim',
 
     'terrortylor/nvim-comment',
+
+    {
+        "kylechui/nvim-surround",
+        version = "*", -- Use for stability; omit to use `main` branch for the latest features
+        event = "VeryLazy",
+        config = function()
+            require("nvim-surround").setup({
+                -- Configuration here, or leave empty to use defaults
+            })
+        end
+    },
+
 })
