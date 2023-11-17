d9fcff6f386fbf717f7b5a03d7c9cb3634ea4454
Jani Haakana <jani.haakana@icloud.com>
2023-11-17 22:29:25 +0200

    fix conform config

--- after/plugin/conform.lua
+++ after/plugin/conform.lua
@@ -1,17 +1,17 @@
-require('conform').setup({
-    formatters_by_ft = {
-        lua = "stylua",
-        python = "black",
-        rust = "rustfmt",
-        javascript = "prettier",
-        typescript = "prettier",
-        json = "fixjson",
-    }
+require("conform").setup({
+	formatters_by_ft = {
+		lua = { "stylua" },
+		python = { "black" },
+		rust = { "rustfmt" },
+		javascript = { "prettier" },
+		typescript = { "prettier" },
+		json = { "fixjson" },
+	},
 })
 
 vim.api.nvim_create_autocmd("BufWritePre", {
-    pattern = "*",
-    callback = function(args)
-        require('conform').format({bufnr = args.bufnr})
-    end,
+	pattern = "*",
+	callback = function(args)
+		require("conform").format({ bufnr = args.bufnr })
+	end,
 })
