d71d651b6f5cbde5d08a3d8fa2f397901aef4ab3
Jani Haakana <jani.haakana@icloud.com>
2023-11-17 19:59:22 +0200

    reindent

--- lua/main/lazy.lua
+++ lua/main/lazy.lua
@@ -1,87 +1,64 @@
 local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    'git',
-    'clone',
-    '--filter=blob:none',
-    'https://github.com/folke/lazy.nvim.git',
-    '--branch=stable', -- latest stable release
-    lazypath,
-  })
+    vim.fn.system({
+        'git',
+        'clone',
+        '--filter=blob:none',
+        'https://github.com/folke/lazy.nvim.git',
+        '--branch=stable', -- latest stable release
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
 require('lazy').setup({
-  {
-	  'nvim-telescope/telescope.nvim', tag = '0.1.4',
-	  -- or                            , branch = '0.1.x',
-	  dependencies = { {'nvim-lua/plenary.nvim'} }
-  },
+    {
+        'nvim-telescope/telescope.nvim', tag = '0.1.4',
+        -- or                            , branch = '0.1.x',
+        dependencies = { {'nvim-lua/plenary.nvim'} }
+    },
 
-  'folke/tokyonight.nvim',
+    'folke/tokyonight.nvim',
 
-  {
-      'folke/trouble.nvim',
-      config = function()
-          require('trouble').setup {
-              icons = false,
-              -- your configuration comes here
-              -- or leave it empty to use the default settings
-              -- refer to the configuration section below
-          }
-      end
-  },
+    {
+        'folke/trouble.nvim',
+        config = function()
+            require('trouble').setup {
+                icons = false,
+                -- your configuration comes here
+                -- or leave it empty to use the default settings
+                -- refer to the configuration section below
+            }
+        end
+    },
 
-  {
-	  'nvim-treesitter/nvim-treesitter',
-	  build = function()
-		  local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
-		  ts_update()
-	  end,
-  },
+    {
+        'nvim-treesitter/nvim-treesitter',
+        build = function()
+            local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+            ts_update()
+        end,
+    },
 
-  'nvim-treesitter/playground',
-  'nvim-lua/plenary.nvim',
-  'theprimeagen/harpoon',
-  'theprimeagen/refactoring.nvim',
-  'mbbill/undotree',
-  'tpope/vim-fugitive',
-  'nvim-treesitter/nvim-treesitter-context',
+    'nvim-treesitter/playground',
+    'nvim-lua/plenary.nvim',
+    'theprimeagen/harpoon',
+    'theprimeagen/refactoring.nvim',
+    'mbbill/undotree',
+    'tpope/vim-fugitive',
+    'nvim-treesitter/nvim-treesitter-context',
 
-  {'williamboman/mason.nvim', lazy = false, config = true},
-  {'williamboman/mason-lspconfig.nvim'},
+    {'williamboman/mason.nvim', lazy = false, config = true},
+    {'williamboman/mason-lspconfig.nvim'},
 
-  {'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
-  {'neovim/nvim-lspconfig'},
-  {'hrsh7th/cmp-nvim-lsp'},
-  {'hrsh7th/nvim-cmp'},
-  {'L3MON4D3/LuaSnip'},
+    {'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
+    {'neovim/nvim-lspconfig'},
+    {'hrsh7th/cmp-nvim-lsp'},
+    {'hrsh7th/nvim-cmp'},
+    {'L3MON4D3/LuaSnip'},
 
---  {
---	  'VonHeikemen/lsp-zero.nvim',
---	  branch = 'v3.x',
---	  dependencies = {
---		  -- LSP Support
---		  {'neovim/nvim-lspconfig'},
---		  {'williamboman/mason.nvim'},
---		  {'williamboman/mason-lspconfig.nvim'},
---
---		  -- Autocompletion
---		  {'hrsh7th/nvim-cmp'},
---		  {'hrsh7th/cmp-buffer'},
---		  {'hrsh7th/cmp-path'},
---		  {'saadparwaiz1/cmp_luasnip'},
---		  {'hrsh7th/cmp-nvim-lsp'},
---		  {'hrsh7th/cmp-nvim-lua'},
---
---		  -- Snippets
---		  {'L3MON4D3/LuaSnip'},
---		  {'rafamadriz/friendly-snippets'},
---	  }
---  },
-
-  'folke/zen-mode.nvim',
-  'github/copilot.vim',
-  'eandrju/cellular-automaton.nvim',
-  'laytan/cloak.nvim',
+    'folke/zen-mode.nvim',
+    'github/copilot.vim',
+    'eandrju/cellular-automaton.nvim',
+    'laytan/cloak.nvim',
 })
