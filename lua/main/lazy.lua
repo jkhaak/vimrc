4d3ad60a2113854cbb0414fd6ec94b0c9f1f12ae
Jani Haakana <jani.haakana@icloud.com>
2023-11-18 19:19:53 +0200

    ad crates.nvim

--- lua/main/lazy.lua
+++ lua/main/lazy.lua
@@ -86,4 +86,13 @@ require('lazy').setup({
     "tpope/vim-repeat",
     'ggandor/lightspeed.nvim',
 
+    {
+    'saecki/crates.nvim',
+    tag = 'v0.4.0',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function()
+        require('crates').setup()
+    end,
+}
+
 })
