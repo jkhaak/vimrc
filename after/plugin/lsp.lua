74f8250ec3ec6b0ce56a5d2244ed0b091ff8d92e
Jani Haakana <jani.haakana@icloud.com>
2023-11-18 12:31:25 +0200

    format lsp file

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,47 +1,47 @@
 local lsp_zero = require("lsp-zero")
 
 lsp_zero.on_attach(function(client, bufnr)
-  -- see :help lsp-zero-keybindings
-  -- to learn the available actions
+    -- see :help lsp-zero-keybindings
+    -- to learn the available actions
 
-  vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-  vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-  vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-  vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-  vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-  vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-  vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-  vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-  vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-  vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
-  lsp_zero.default_keymaps({buffer = bufnr})
+    vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+    vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+    vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+    vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+    vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+    vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+    vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+    vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+    vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+    vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+    lsp_zero.default_keymaps({ buffer = bufnr })
 end)
 
 vim.g.autoformat = 1
 
 vim.api.nvim_create_autocmd("BufWritePre", {
-	pattern = "*",
-	callback = function(args)
+    pattern = "*",
+    callback = function(args)
         if vim.g.autoformat > 0 then
             vim.lsp.buf.format()
         end
-	end,
+    end,
 })
 
 require('mason-lspconfig').setup({
     ensure_installed = {
-      'tsserver',
-      'rust_analyzer',
+        'tsserver',
+        'rust_analyzer',
     },
     handlers = {
         lsp_zero.default_setup,
     }
 })
 
-lsp_zero.setup_servers({'lua_ls', 'tsserver', 'rust_analyzer'})
+lsp_zero.setup_servers({ 'lua_ls', 'tsserver', 'rust_analyzer' })
 
 local cmp = require('cmp')
-local cmp_select = {behavior = cmp.SelectBehavior.Select}
+local cmp_select = { behavior = cmp.SelectBehavior.Select }
 
 require('luasnip.loaders.from_vscode').lazy_load()
 
@@ -50,7 +50,7 @@ cmp.setup({
         { name = 'path' },
         { name = 'nvim_lsp' },
         { name = 'nvim_lua' },
-        { name = 'buffer', keyword_length = 3 },
+        { name = 'buffer',  keyword_length = 3 },
         { name = 'luasnip', keyword_length = 2 },
     },
 
@@ -61,4 +61,3 @@ cmp.setup({
         ["<C-Space>"] = cmp.mapping.complete(),
     })
 })
-
