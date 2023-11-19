6656e59d3c6f246799c0cb60956c7dfe95b3f052
Jani Haakana <jani.haakana@icloud.com>
2023-11-19 18:04:12 +0200

    disable italics

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -8,8 +8,8 @@ require("tokyonight").setup({
     styles = {
         -- Style to be applied to different syntax groups
         -- Value is any valid attr-list value for `:help nvim_set_hl`
-        comments = { italic = true },
-        keywords = { italic = true },
+        comments = { italic = false },
+        keywords = { italic = false },
         functions = {},
         variables = {},
         -- Background styles. Can be "dark", "transparent" or "normal"
@@ -25,13 +25,13 @@ require("tokyonight").setup({
     --- You can override specific color groups to use other groups or a hex color
     --- function will be called with a ColorScheme table
     ---@param colors ColorScheme
-    on_colors = function(colors) end,
+    -- on_colors = function(colors) end,
 
     --- You can override specific highlights to use other groups or a hex color
     --- function will be called with a Highlights and ColorScheme table
     ---@param highlights Highlights
     ---@param colors ColorScheme
-    on_highlights = function(highlights, colors) end,
+    -- on_highlights = function(highlights, colors) end,
 })
 
 vim.cmd("colorscheme tokyonight")
